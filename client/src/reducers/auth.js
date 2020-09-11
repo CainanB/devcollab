a8d43f8b0fd95681597694b4bdec4bb8903e5bc6
Cainan Barboza <CainanBarboza1@gmail.com>
2020-09-11 11:38:14 -0500

    logout functionality with homepage redirect

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,8 @@ import{
     USER_LOADED,
     AUTH_ERROR,
     LOGIN_SUCCESS,
-    LOGIN_FAIL
+    LOGIN_FAIL,
+    LOGOUT
 
 } from '../actions/constants'
 
@@ -37,6 +38,7 @@ export default function(state = initialState, action){
         case REGISTER_FAIL:
         case AUTH_ERROR:
         case LOGIN_FAIL:
+        case LOGOUT:
             localStorage.removeItem('token');
             return {
                 ...state,
