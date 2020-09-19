20b5cdbe8a39bddfab7f788bab4975a626871a24
Micah Peterson <micahpeterson@live.com>
2020-09-19 15:50:04 -0500

    comment stlying

--- client/src/views/examples/Comment.js
+++ client/src/views/examples/Comment.js
@@ -14,9 +14,9 @@ const Comment = ({commentId, postId, name, user, text, auth, deleteComment}) => 
         <Row>
             <Col className="d-flex justify-content-space-between">
 
-                <img height='25px' width='25px' src={user.profileimg} /><span> {name}</span><p> {text}</p> 
+                <img height='25px' width='25px' src={user.profileimg} /><span className="pl-2"> {name}</span><p className="pl-4"> {text}</p> 
                 {!auth.loading && user === auth.user._id ? (
-                    <Button size="sm" onClick={()=>deleteComment(commentId,postId)}>X</Button>
+                    <Button className="ml-2" style={{marginTop:'-5px'}} size="sm" onClick={()=>deleteComment(commentId,postId)}>X</Button>
                 ):null}
 
             </Col>
